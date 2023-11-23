cfef188bf1365ffde180626547da8f72e3df7418
ShubhamJoshi1998 <shivam306joshi@gmail.com>
2023-11-23 10:30:21 +0530

    server creation & path routing

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-// const lib = require("./lib.js")
-import { sum, diff } from "./lib.js"
-// const fs = require('fs');
-import fs from 'fs'
 
-//const txt = fs.readFileSync('demo.txt', 'utf-8');
-//console.log(txt)
-fs.readFile('demo.txt', 'utf-8', (err, txt) => {
-    console.log(txt)
+// const express = require('express');
+
+// console.log("hello moye moye")
+
+// const server = express();
+// server.listen(8080);
+
+const http = require('http');
+const fs = require('fs');
+
+const index = fs.readFileSync('index.html', 'utf-8');
+const data = fs.readFileSync('data.json', 'utf-8');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/html');
+            res.end(index);
+            break;
+        case '/api':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(data);
+            break;
+        default:
+            res.writeHead(404);
+            res.end();
+    }
+
+    console.log("server start");
+
+
 });
 
-
-console.log("lib", sum(4,5), diff(5,2))
-
-const name = 'Raj'
-const age = 20
-const location = 'India'
-
-// User with ES6 shorthand 
-// property  
-const user = {
-    name,
-    age,
-    location
-}
-
-console.log(user) 
+server.listen(8080);
