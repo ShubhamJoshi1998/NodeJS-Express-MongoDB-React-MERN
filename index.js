6870c4f24ecea41bba592873335dd1441d9d63b1
ShubhamJoshi1998 <shivam306joshi@gmail.com>
2023-11-23 11:20:07 +0530

    server rendering

--- index.js
+++ index.js
@@ -10,10 +10,34 @@ const http = require('http');
 const fs = require('fs');
 
 const index = fs.readFileSync('index.html', 'utf-8');
-const data = fs.readFileSync('data.json', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products;
+
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    console.log(req.url, req.method)
+
+    if(req.url.startsWith('/product')){
+        const id = req.url.split('/')[2]
+        const product = products.find(p=>p.id===(+id))
+        console.log(product)
+        res.setHeader('Content-Type', 'text/html');
+              let modifiedIndex = index.replace('**title**', product.title)
+              .replace('**url**', product.thumbnail)
+              .replace('**price**', product.price)
+              .replace('**rating**', product.rating)
+              res.end(modifiedIndex);
+              return;
+      }
+
+    //   case '/product':
+    //     res.setHeader('Content-Type', 'text/html');
+    //     let modifiedIndex = index.replace('**title**', product.title)
+    //         .replace('**url**', product.thumbnail)
+    //         .replace('**price**', product.price)
+    //         .replace('**rating**', product.rating)
+    //     res.end(modifiedIndex);
+    //     break;
     switch (req.url) {
         case '/':
             res.setHeader('Content-Type', 'text/html');
@@ -21,7 +45,7 @@ const server = http.createServer((req, res) => {
             break;
         case '/api':
             res.setHeader('Content-Type', 'application/json');
-            res.end(data);
+            res.end(JSON.stringify(data));
             break;
         default:
             res.writeHead(404);
