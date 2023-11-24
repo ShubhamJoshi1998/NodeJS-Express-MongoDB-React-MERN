27f346eb2fb14e6aa02a93ace6d4efe7db07602f
ShubhamJoshi1998 <shivam306joshi@gmail.com>
2023-11-24 11:55:25 +0530

    REST API

--- index.js
+++ index.js
@@ -3,39 +3,69 @@ const index = fs.readFileSync('index.html', 'utf-8');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 const products = data.products;
 
-
 const express = require('express');
+const morgan = require('morgan');
 const server = express();
 
-server.get('/', (req,res) => {
-    res.json({type:'GET'})
-})
-server.post('/', (req,res) => {
-    res.json({type:'POST'})
-})
-server.put('/', (req,res) => {
-    res.json({type:'PUT'})
-})
-server.delete('/', (req,res) => {
-    res.json({type:'DELETE'})
-})
-server.patch('/', (req,res) => {
-    res.json({type:'PATCH'})
-})
+//bodyParser
+server.use(express.json());
+server.use(morgan('dev'))
+server.use(express.static('public'));
 
+// API - Endpoint - Route
 
+// Create POST  /products   C R U D
+server.post('/products', (req, res) => {
+    console.log(req.body);
+    products.push(req.body);
+    res.json(req.body);
+});
 
+// Read GET /products
+server.get('/products', (req, res) => {
+    res.json(products);
+});
+//Read GET /products/:id
+server.get('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const product = products.findIndex(p => p.id === id)
+    products.splice(productIndex, 1, { ...req.body, id: id })
+    // res.json(product);
+});
 
+//Update PUT /products/:id
+server.put('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id)
+    products.splice(productIndex, 1, { ...req.body, id: id })
+    res.status(201).json();
+});
 
-server.get('/', (req,res) => {
-    //res.send('<h1>Hello</h1>')
-    //res.sendFile('D:\Shubham\Node\node-app\index.html')
-    // res.json(products);
-    res.sendStatus(404);
-})
+//Update PATCH /products/:id
+server.patch('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id)
+    const product = products[productIndex];
+    products.splice(productIndex, 1, { ...product, ...req.body })
+    res.status(201).json();
+});
 
+// Delete DELETE /products/:id
+server.delete('/products/:id', (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id)
+    const product = products[productIndex];
+    products.splice(productIndex, 1)
+    res.status(201).json(product);
+});
+
+server.get('/demo', (req, res) => {
+    // res.sendStatus(404);
+    // res.json(products)
+    // res.status(201).send('<h1>hello</h1>')
+    // res.sendFile('/Users/abhishekrathore/Desktop/node-app/index.html')
+});
 
 server.listen(8080, () => {
-    console.log("express server started");
-})
-
+    console.log('server started');
+});
